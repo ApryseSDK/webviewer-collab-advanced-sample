0c673a72908fe516b04a07ce62a0df5d77c8cdeb
Logan Bittner <lbittner@pdftron.com>
2020-11-03 13:19:04 -0800

    More theme changes

--- client/theme.ts
+++ client/theme.ts
@@ -4,7 +4,9 @@ import { deepMerge } from 'grommet/utils'
 export default deepMerge(grommet, {
   global: {
     colors: {
-      brand: '#00a5e4'
+      brand: '#00a5e4',
+      'neutral-2': '#3a5169',
+      'accent-1': '#face00'
     }
   }
 })
