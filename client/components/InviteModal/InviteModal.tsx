46f2a6e636856296a5468d91f3032627e6080eaf
Jackie Huang <jhuang@pdftron.com>
2021-03-03 07:11:16 +0800

    update InviteModal

--- client/components/InviteModal/InviteModal.tsx
+++ client/components/InviteModal/InviteModal.tsx
@@ -1,46 +1,36 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Modal from '../Modal';
 import InviteList from '../InviteList';
 import { useSelector } from 'react-redux';
 import { getClient } from '../../redux/viewer';
 import { toast } from 'react-toastify';
 
-export default ({ onExit, document }) => {
+export default ({
+  onExit,
+  document
+}) => {
+
   const client = useSelector(getClient);
-  let success = true;
 
-  useEffect(() => {
-    const unsub = client.subscribe('permissionError', (type, action) => {
-      if (type === 'Document' && action === 'invite') {
-        success = false;
-      }
-    });
-    return () => {
-      unsub();
-    };
-  }, [client, document]);
-
-  const submit = useCallback(
-    async (list) => {
-      if (list.length === 0) {
-        toast.error('You must invite at least one user');
-        onExit();
-        return;
-      }
-      await client.inviteUsersToDocument(document.id, list);
-      if (success) {
-        toast.success('Success!');
-      } else {
-        toast.error('Error: No permission or user is already invited.');
-      }
+  const submit = useCallback(async (list) => {
+    if (list.length === 0) {
+      toast.error('You must invite at least one user')
       onExit();
-    },
-    [client, document]
-  );
+      return;
+    }    
+    try {
+      await client.inviteUsersToDocument(document.id, list);
+      toast.success('Success!');
+    } catch (e) {
+      toast.error('Error: No permission or user is already invited.');
+    }
+    onExit();
+  }, [client, document])
 
   return (
     <Modal onExit={onExit}>
       <InviteList onSubmit={submit} />
     </Modal>
-  );
-};
+  )
+
+}
